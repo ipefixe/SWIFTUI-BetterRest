98885ff79fa71fd47f1c9838f726e450a8ff977b
Ipefixe <ipefixe.fr@gmail.com>
2021-01-30 21:07:59 +0100

    Initial Commit

---
+++ Better Rest/Better_RestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Better_RestApp.swift
+//  Better Rest
+//
+//  Created by Kevin Boulala on 30/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct Better_RestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
